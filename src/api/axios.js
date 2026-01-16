45aa37655e417746b8c6eb587f315dae7f64f02d
Samuel Alcon <samuelalcon0802@gmail.com>
2026-01-16 02:22:01 -0400

    Cambio para la web y local

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-// Ajusta el puerto aquí según tu launchSettings.json (veo que usas 7069 para HTTPS)
-const BASE_URL = 'https://localhost:7069/api';
+// Esto es lo que hace la magia:
+// Dice: "Si existe una variable de entorno en la nube, úsala. Si no, usa localhost."
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://localhost:7069/api';
 
 export default axios.create({
     baseURL: BASE_URL,
-    withCredentials: true // ¡IMPORTANTE! Permite enviar/recibir Cookies de sesión
+    withCredentials: true 
 });
